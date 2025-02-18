bfe1d0b00457b098227ba10d3fbbcb67b589ed0f
AbdulWashid <abdul.washid2004@gmail.com>
2025-02-18 08:40:53 +0530

    qr ode in wallet recharge

--- app/Http/Controllers/userController/WalletController.php
+++ app/Http/Controllers/userController/WalletController.php
@@ -6,13 +6,19 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Wallet;
 use Auth;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class WalletController extends Controller
 {
     public function index(){
         $wallet = Wallet::where('user_id', Auth::id())->get();
         $userdata = Auth::user();
-        return view('user.recharge', compact('wallet','userdata'));
+        $qrCode = QrCode::size(150)
+                // ->color(255, 0, 0) // Red color
+                // ->backgroundColor(255, 255, 0) // Yellow background
+                // ->margin(10)
+                ->generate('upi://pay?pa=abdulwashid25@okicici&pn=Abdul%20Washid&cu=INR&tn=Payment%20for%20Service');
+        return view('user.recharge', compact('wallet','userdata','qrCode'));
     }
 
     public function payment(Request $request){
